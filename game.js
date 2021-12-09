1de8d037f5a0b0b5340298711981235713dde333
aidanliddiard <aidanliddiard@gmail.com>
2021-12-09 14:23:50 -0800

    throws and computer pics

--- game.js
+++ game.js
@@ -20,5 +20,17 @@ export function score(player, computer) {
     if (player === 'scissors' && computer === 'paper') {
         return 'player is the winner';
     }
-    
+}
+
+const throws = ['rock', 'paper', 'scissors'];
+
+export function generateThrow() {
+    const index = getRandomIntInclusive(0, 2);
+    return throws[index];
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
